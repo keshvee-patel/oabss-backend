b4147d1b8c9ccba3c527a19a4e30ff16c6411883
Keshvee Patel <keshvee.p@somaiya.edu>
2025-06-17 14:37:00 +0530

    Fix: use dynamic port for Render

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
